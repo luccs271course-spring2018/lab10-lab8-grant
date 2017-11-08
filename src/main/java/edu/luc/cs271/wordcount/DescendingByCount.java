18f7b8c8d9fd6970abd881d9c3cff91967e08a25
Grant Smith <smith9327@yahoo.com>
2017-11-07 23:01:31 -0600

    initial import

--- src/main/java/edu/luc/cs271/wordcount/DescendingByCount.java
+++ src/main/java/edu/luc/cs271/wordcount/DescendingByCount.java
@@ -8,6 +8,7 @@ public class DescendingByCount implements Comparator<Map.Entry<String, Integer>>
   public int compare(final Map.Entry<String, Integer> l, final Map.Entry<String, Integer> r) {
 
     // TODO turn this into descending order using the getValue() method on l and r
+    //importing
     return -1;
 
   }
